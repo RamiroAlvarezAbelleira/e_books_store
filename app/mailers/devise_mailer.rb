f2eb7fcd23323c618237abf5337027ce2003766a
ramiroalvarezabelleira <alvarezramiro1997@gmail.com>
2023-10-10 17:14:24 -0300

    added procfile

--- app/mailers/devise_mailer.rb
+++
@@ -1,4 +0,0 @@
-class DeviseMailer < Devise::Mailer
-    include Devise::Controllers::UrlHelpers # Include Devise URL helpers
-    default template_path: 'devise/mailer' # Set the template path
-end
