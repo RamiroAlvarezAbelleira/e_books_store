de3ba61b404fb1777c61bd4cbabf31e1af7afc43
ramiroalvarezabelleira <alvarezramiro1997@gmail.com>
2023-10-10 15:42:59 -0300

    testing

--- app/sidekiq/update_book_prices_job.rb
+++ app/sidekiq/update_book_prices_job.rb
@@ -4,12 +4,13 @@ class UpdateBookPricesJob
   def perform(publisher_id, type_of_update, percentage)
     publisher = Publisher.find(publisher_id)
     books = publisher.books
-    puts 'entre al job' + publisher_id
+    puts "entre al job #{publisher_id}"
     if type_of_update == 'increase'
       books.each { |book| book.update(price: book.price * (1 + (percentage / 100))) }
     elsif type_of_update == 'decrease'
       books.each { |book| book.update(price: book.price * (1 - (percentage / 100))) }
     end
+    puts "despues del job"
 
     PriceUpdateStatus.instance.update(price_update_completed: true)
   end
